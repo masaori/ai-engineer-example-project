673a37c838c2df236ff633f4ce60a88e4ecce1b8
Masaori Hirono <masaori.hirono@gmail.com>
2023-08-08 12:09:49 -0500

    Add test file for CreateUserUsecase

---
+++ src/domain/usecases/CreateUserUsecase.test.ts
@@ -0,0 +1,23 @@
+describe('CreateUserUsecase', () => {
+    test('should create user successfully', () => {
+        // Arrange
+        // TODO: Instantiate CreateUserUsecase with mock dependencies
+
+        // Act
+        // TODO: Call CreateUserUsecase.execute with valid input
+
+        // Assert
+        // TODO: Expect that the user was created successfully
+    });
+
+    test('should throw error when input is invalid', () => {
+        // Arrange
+        // TODO: Instantiate CreateUserUsecase with mock dependencies
+
+        // Act
+        // TODO: Call CreateUserUsecase.execute with invalid input
+
+        // Assert
+        // TODO: Expect that an error was thrown
+    });
+});

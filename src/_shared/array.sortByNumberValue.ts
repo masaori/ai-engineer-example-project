bad4e5e869436746f5a077a00f5c4a93245c0d56
Masaori Hirono <masaori.hirono@gmail.com>
2023-08-22 15:52:14 -0500

    delete unnecessary files

--- src/_shared/array.sortByNumberValue.ts
+++
@@ -1,3 +0,0 @@
-import sortByComparableValue from './array.sortByComparableValue';
-
-export const sortByNumberValue = (a: number, b: number, order: 'asc' | 'desc') => sortByComparableValue(a, b, order);
--- src/_shared/array.sortByStringValue.ts
+++
@@ -1,3 +0,0 @@
-import sortByComparableValue from './array.sortByComparableValue';
-
-export const sortByStringValue = (a: string, b: string, order: 'asc' | 'desc') => sortByComparableValue(a, b, order);
